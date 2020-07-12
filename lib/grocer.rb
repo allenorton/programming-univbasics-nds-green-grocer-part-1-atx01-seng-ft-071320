1b034509fd50bc90ff6813feeb6fa15093bf4c70
Allene Norton <norton.allene@gmail.com>
2020-07-12 20:31:24 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -18,7 +18,7 @@ def consolidate_cart(cart)
     food_hash.each do |key, value|
       if food_hash[:key] == find_item_by_name_in_collection(food_hash[:key], cart)
         nu_cart << food_hash
-        nu_cart.flatten.uniq 
+        cons_cart = nu_cart.flatten.uniq 
       end 
     end 
   end 
